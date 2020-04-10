ac4c196a286807439ee54c95708f203ee54defc7
Raznor <62889081+Raznor@users.noreply.github.com>
2020-04-10 18:10:44 +0200

    solved second jQuery exercise

--- scripts.js
+++ scripts.js
@@ -6,3 +6,12 @@ myNames.forEach(name => {
         $("#Raznor").css("font-weight", "bold");
     }
 });
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+};
+
+$("body").append(
+    '<h1>' + additionalBlock.title + '</h1><p>' + additionalBlock.text + '</p>'
+)
